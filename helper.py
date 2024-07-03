6d9e1b54f62014464078af5ef03db726c1fdd5ea
Herpakka <106274468+Herpakka@users.noreply.github.com>
2024-07-03 19:32:11 +0700

    0307
    oop of my playground

---
+++ helper.py
@@ -0,0 +1,114 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+from mediapipe.framework.formats import landmark_pb2
+
+def resize_with_aspect_ratio(image, width=None, height=None):
+    # Get the original image dimensions
+    h, w = image.shape[:2]
+
+    # Calculate the aspect ratio
+    aspect_ratio = w / h
+
+    if width is None:
+        # Calculate height based on the specified width
+        new_height = int(height / aspect_ratio)
+        resized_image = cv2.resize(image, (height, new_height))
+    else:
+        # Calculate width based on the specified height
+        new_width = int(width * aspect_ratio)
+        resized_image = cv2.resize(image, (new_width, width))
+
+    return resized_image
+
+def posterize(image):
+    spatial_radius = 20
+    color_radius = 10
+    num_levels = 40
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert to RGB
+    # Apply pyrMeanShiftFiltering
+    filtered_image = cv2.pyrMeanShiftFiltering(image, spatial_radius, color_radius)
+
+    # Quantize the colors
+    quantized_image = np.floor(filtered_image / (256 / num_levels)) * (256 / num_levels)
+    quantized_image = np.uint8(quantized_image)
+    quantized_image = cv2.cvtColor(quantized_image, cv2.COLOR_RGB2BGR)  # Convert to BGR
+
+    return quantized_image
+
+def sort_points_clockwise(points):
+    # Calculate the centroid of the points
+    centroid = np.mean(points, axis=0)
+    # Sort points based on the angle with respect to the centroid
+    sorted_points = sorted(points, key=lambda point: np.arctan2(point[1] - centroid[1], point[0] - centroid[0]))
+    return np.array(sorted_points, dtype=np.int32)
+
+def draw_landmarks_Tzone(rgb_image, face_landmarks_list):
+    annotated_image = np.copy(rgb_image)
+    face_rect = []
+
+    # Loop through the detected faces to visualize.
+    for face_landmarks in face_landmarks_list:
+        # Draw the face landmarks.
+        mask = np.zeros_like(rgb_image)
+        face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        face_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks.landmark
+        ])
+        
+        # face polygon
+        face_indices = [i for t in mp.solutions.face_mesh.FACEMESH_FACE_OVAL for i in t]
+        face_polygon = np.array([(int(face_landmarks.landmark[i].x * rgb_image.shape[1]), 
+                                  int(face_landmarks.landmark[i].y * rgb_image.shape[0])) for i in face_indices], dtype=np.int32)
+        face_polygon = sort_points_clockwise(face_polygon) # เรียง polygon ใบหน้าตามเข็มนาฬิกา
+        
+        # T-zone polygon
+        Tzone_indices = [152,148,176,140,32,194,182,181,167,45,51,3,196,122,193,55,65,52,53,63,68,54,103,67,109,10,338,297,332,284,298,293,283,282,295,285,417,351,419,248,281,275,393,405,406,418,262,369,400,377]
+        Tzone_polygon = np.array([(int(face_landmarks.landmark[i].x * rgb_image.shape[1]), 
+                                  int(face_landmarks.landmark[i].y * rgb_image.shape[0])) for i in Tzone_indices], dtype=np.int32)
+        
+        # L cheek polygon
+        Lchk_indices = [143,121,126,203,207,147]
+        Lchk_polygon = np.array([(int(face_landmarks.landmark[i].x * rgb_image.shape[1]), 
+                                  int(face_landmarks.landmark[i].y * rgb_image.shape[0])) for i in Lchk_indices], dtype=np.int32)
+        
+        # L cheek polygon
+        Rchk_indices = [372,350,355,423,427,376]
+        Rchk_polygon = np.array([(int(face_landmarks.landmark[i].x * rgb_image.shape[1]), 
+                                  int(face_landmarks.landmark[i].y * rgb_image.shape[0])) for i in Rchk_indices], dtype=np.int32)
+        
+        # eyes polygon
+        L_eye_indices = [i for t in mp.solutions.face_mesh.FACEMESH_LEFT_EYE for i in t]
+        L_eye_polygon = np.array([(int(face_landmarks.landmark[i].x * rgb_image.shape[1]), 
+                                  int(face_landmarks.landmark[i].y * rgb_image.shape[0])) for i in L_eye_indices], dtype=np.int32)
+        L_eye_polygon = sort_points_clockwise(L_eye_polygon) # เรียง polygon ตาซ้ายตามเข็มนาฬิกา
+        R_eye_indices = [i for t in mp.solutions.face_mesh.FACEMESH_RIGHT_EYE for i in t]
+        R_eye_polygon = np.array([(int(face_landmarks.landmark[i].x * rgb_image.shape[1]), 
+                                  int(face_landmarks.landmark[i].y * rgb_image.shape[0])) for i in R_eye_indices], dtype=np.int32)
+        R_eye_polygon = sort_points_clockwise(R_eye_polygon) # เรียง polygon ตาขวาตามเข็มนาฬิกา
+        
+        # mouth polygon
+        mouth_indices = [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291, 375, 321, 405, 314, 17, 84, 181, 91, 146]
+        mouth_polygon = np.array([(int(face_landmarks.landmark[i].x * rgb_image.shape[1]), 
+                                  int(face_landmarks.landmark[i].y * rgb_image.shape[0])) for i in mouth_indices], dtype=np.int32)
+        mouth_polygon = sort_points_clockwise(mouth_polygon) # เรียง polygon ปากตามเข็มนาฬิกา
+        
+        # Find bounding box of face polygon
+        x, y, w, h = cv2.boundingRect(face_polygon)
+        print("face rect = ",x,y,w,h)
+        face_rect.append((x, y, w, h))  # Store bounding box coordinates
+        
+        # Fill the face polygon on the mask
+        cv2.fillPoly(mask, [Tzone_polygon], (255,255,255))
+        cv2.fillPoly(mask, [Rchk_polygon], (255,255,255))
+        cv2.fillPoly(mask, [Lchk_polygon], (255,255,255))
+        # cv2.fillPoly(mask, [face_polygon], (255, 255, 255))
+        # cv2.fillPoly(mask, [L_eye_polygon], (0, 0, 0))
+        # cv2.fillPoly(mask, [R_eye_polygon], (0, 0, 0))
+        cv2.fillPoly(mask, [mouth_polygon], (0, 0, 0))
+        # Black out the area outside the face
+        annotated_image = cv2.bitwise_and(annotated_image, mask)
+        
+        annotated_image = annotated_image[y:y + h, x:x + w]
+
+    return annotated_image
