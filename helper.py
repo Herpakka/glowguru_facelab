55e4baf496c1ff2f3ed25e78b98448d4c37234a9
Herpakka <106274468+Herpakka@users.noreply.github.com>
2024-07-05 15:58:30 +0700

    add some function

--- helper.py
+++ helper.py
@@ -43,7 +43,24 @@ def sort_points_clockwise(points):
     sorted_points = sorted(points, key=lambda point: np.arctan2(point[1] - centroid[1], point[0] - centroid[0]))
     return np.array(sorted_points, dtype=np.int32)
 
-def draw_landmarks_Tzone(rgb_image, face_landmarks_list):
+def use_clahe(img,clahe):
+    image_bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    image_bw_re = resize_with_aspect_ratio(image_bw, width=500)
+    final_img = image_bw_re.copy()
+    final_img = clahe.apply(image_bw_re) + 30 # default 30
+    
+    return final_img
+
+def find_contours(img):
+    edged = cv2.Canny(img, 30, 200)
+    contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return len(contours)
+
+def naming_code(og_name,allC_id,foreC_id,chkC_id):
+    sp = "_"
+    return og_name+"C"+allC_id+sp+foreC_id+sp+chkC_id
+
+def draw_landmarks_Tzone(rgb_image, face_landmarks_list, mode):
     annotated_image = np.copy(rgb_image)
     face_rect = []
 
@@ -66,6 +83,12 @@ def draw_landmarks_Tzone(rgb_image, face_landmarks_list):
         Tzone_indices = [152,148,176,140,32,194,182,181,167,45,51,3,196,122,193,55,65,52,53,63,68,54,103,67,109,10,338,297,332,284,298,293,283,282,295,285,417,351,419,248,281,275,393,405,406,418,262,369,400,377]
         Tzone_polygon = np.array([(int(face_landmarks.landmark[i].x * rgb_image.shape[1]), 
                                   int(face_landmarks.landmark[i].y * rgb_image.shape[0])) for i in Tzone_indices], dtype=np.int32)
+        
+        # forehead polygon
+        fore_indices = [55,65,52,53,63,68,54,103,67,109,10,338,297,332,284,298,293,283,282,295,285,8]
+        fore_polygon = np.array([(int(face_landmarks.landmark[i].x * rgb_image.shape[1]), 
+                                  int(face_landmarks.landmark[i].y * rgb_image.shape[0])) for i in fore_indices], dtype=np.int32)
+        
         
         # L cheek polygon
         Lchk_indices = [143,121,126,203,207,147]
@@ -95,17 +118,25 @@ def draw_landmarks_Tzone(rgb_image, face_landmarks_list):
         
         # Find bounding box of face polygon
         x, y, w, h = cv2.boundingRect(face_polygon)
-        print("face rect = ",x,y,w,h)
         face_rect.append((x, y, w, h))  # Store bounding box coordinates
         
         # Fill the face polygon on the mask
-        cv2.fillPoly(mask, [Tzone_polygon], (255,255,255))
-        cv2.fillPoly(mask, [Rchk_polygon], (255,255,255))
-        cv2.fillPoly(mask, [Lchk_polygon], (255,255,255))
-        # cv2.fillPoly(mask, [face_polygon], (255, 255, 255))
-        # cv2.fillPoly(mask, [L_eye_polygon], (0, 0, 0))
-        # cv2.fillPoly(mask, [R_eye_polygon], (0, 0, 0))
-        cv2.fillPoly(mask, [mouth_polygon], (0, 0, 0))
+        if mode == "allface":
+            cv2.fillPoly(mask, [face_polygon], (255, 255, 255))
+            cv2.fillPoly(mask, [L_eye_polygon], (0, 0, 0))
+            cv2.fillPoly(mask, [R_eye_polygon], (0, 0, 0))
+            cv2.fillPoly(mask, [mouth_polygon], (0, 0, 0))
+        elif mode == "Tzone":
+            cv2.fillPoly(mask, [Tzone_polygon], (255,255,255))
+            cv2.fillPoly(mask, [Rchk_polygon], (255,255,255))
+            cv2.fillPoly(mask, [Lchk_polygon], (255,255,255))
+            cv2.fillPoly(mask, [mouth_polygon], (0, 0, 0))
+        elif mode == "fore":
+            cv2.fillPoly(mask, [fore_polygon], (255, 255, 255))
+        elif mode == "chk":
+            cv2.fillPoly(mask, [Rchk_polygon], (255,255,255))
+            cv2.fillPoly(mask, [Lchk_polygon], (255,255,255))
+        
         # Black out the area outside the face
         annotated_image = cv2.bitwise_and(annotated_image, mask)
         
